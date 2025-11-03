c5c8d1fe5bc53007c979ae203be55a7b2510e2ea
Starosadcha <s32262@pjwstk.edu.pl>
2025-11-04 00:06:03 +0100

    11.2

--- Assets/New Folder/MovingPlatform.cs
+++ Assets/New Folder/MovingPlatform.cs
@@ -4,57 +4,25 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Transform pointA; // начальная точка
-    public Transform pointB; // конечная точка
-    public float speed = 2f;
-
-    private bool playerOn = false;
-    private Vector3 target;
-
+    private Animator anim;
     void Start()
     {
-        if (pointA == null || pointB == null)
-        {
-           
-            enabled = false;
-            return;
-        }
-
-        transform.position = pointA.position;
-        target = pointA.position;
+        anim = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // движение
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        // если дошла до цели — стоим
-        if (Vector3.Distance(transform.position, target) < 0.05f)
-        {
-            if (playerOn)
-                target = pointB.position; // если игрок на платформе — едем к B
-            else
-                target = pointA.position; // если игрок ушёл — возвращаемся домой
-        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerOn = true;
-            target = pointB.position;
-            other.transform.SetParent(transform); // чтобы игрок ехал вместе
-        }
+        anim.SetBool("IsOpen", true);
     }
-
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerOn = false;
-            target = pointA.position;
-            other.transform.SetParent(null); // открепляем игрока
-        }
+        anim.SetBool("IsOpen", false);
     }
+}
