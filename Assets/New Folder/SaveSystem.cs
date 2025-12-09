ed9ee1d8816721a613f22095780479966a4f6dc7
Starosadcha <s32262@pjwstk.edu.pl>
2025-12-09 17:16:21 +0100

    12.09

---
+++ Assets/New Folder/SaveSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour
+{
+    private static string fileName = "savegame.json";
+    private static string GetPath()
+    {
+        return Path.Combine(Application.persistentDataPath, fileName);
+    }
+
+    public static void Save(SaveData data)
+    {
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(GetPath(), json);
+        Debug.Log("Game saved to" + GetPath());
+    }
+    public static SaveData Load()
+    {
+        string path = GetPath(); 
+        if (!File.Exists(path))
+        {
+            Debug.Log("Brak zapisu gry");
+            return null;
+        }
+        string json = File.ReadAllText(path);
+        SaveData data = JsonUtility.FromJson<SaveData>(json);  
+        Debug.Log("Game loaded from" + path);
+        return data;
+    }
+}
